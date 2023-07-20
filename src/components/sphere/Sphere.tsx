72b4170862ddb2c507bce3c5db87d4fec2d6ca8c
Nicole <petovellon@gmail.com>
2023-07-20 15:24:24 +0200

    Created reusable sphere component for images

---
+++ src/components/sphere/Sphere.tsx
@@ -0,0 +1,25 @@
+import "./Sphere.css";
+import { SphereProps } from "../../models/SphereProps";
+
+const Sphere: React.FC<SphereProps> = (props: SphereProps) => {
+	return (
+		<span
+			className="sphere"
+			style={{
+				backgroundColor: props.backgroundColor,
+				width: props.width,
+				height: props.height,
+			}}
+		>
+			<img
+				src={props.src}
+				alt="A cutting board with veggies on top"
+				className="food"
+				width={`${props.width} px`}
+				height={`${props.height} px`}
+			/>
+		</span>
+	);
+};
+
+export default Sphere;
---
+++ src/models/SphereProps.ts
@@ -0,0 +1,9 @@
+export type SphereProps = {
+    backgroundColor: string | undefined;
+    opacity?: number;
+    width: number;
+    height: number;
+    src: string;
+    bottom?: number;
+    top?: number
+}
