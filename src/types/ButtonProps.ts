93f2971d8203f2a6eff2bcaf13933418a04d9861
Nicole <petovellon@gmail.com>
2023-07-25 13:13:55 +0200

    Renamed models folder to types

--- src/types/ButtonProps.ts
+++ src/types/ButtonProps.ts
@@ -2,7 +2,7 @@ export type ButtonProps = {
     title: string;
     onClick?: () => void | Promise<void>; // optional, but recommended to handle async operations
     //For the places where we will use green buttons
-    isGreen: boolean;
+    color: string;
     //FIXME: add correct typing
     icon?: any;
 }
