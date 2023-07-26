f344cc2564e02d52d495168934b81c7e8c4602b5
Nicole <petovellon@gmail.com>
2023-07-26 16:59:20 +0200

    Created input/textarea components

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,16 @@
+import { InputProps } from "../../types/InputProps";
+import "./Input.css";
+
+const Input: React.FC<InputProps> = (props: InputProps) => {
+	return (
+		<div className="input-container">
+			{props.multiline ? (
+				<textarea className="input multiline" placeholder={props.placeholder} aria-label="A text area to paste a recipe" />
+			) : (
+				<input className="input singleline" placeholder={props.placeholder}   aria-label="An input to paste a url" type="text" />
+			)}
+		</div>
+	);
+};
+
+export default Input;
