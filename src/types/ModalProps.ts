d3da2d5b7e59abdab0082757a83826afe4da6063
Nicole <petovellon@gmail.com>
2023-08-01 15:37:07 +0200

    Created modal component with its props

---
+++ src/types/ModalProps.ts
@@ -0,0 +1,9 @@
+import { ReactComponentElement } from "react";
+
+export type ModalProps = {
+    isOpen?: boolean,
+    modalTitle: string,
+    //TODO: type this more specifically
+    children?: ReactComponentElement<any, any>[];
+    toggable?: boolean
+}
