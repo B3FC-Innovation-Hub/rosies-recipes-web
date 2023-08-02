17e375213e047d80fffd4d22a1723d94502d927d
Nicole <petovellon@gmail.com>
2023-08-02 11:29:40 +0200

    Fixed build

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/login/Login";
-import Test from "../pages/login/Test";
 
 const RoutesContainer: React.FC = () => {
 	return(
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Login />} />
-				<Route path="/test" element={<Test />} />
 			</Routes>
 		</BrowserRouter>
 	);
