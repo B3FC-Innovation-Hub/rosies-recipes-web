207da47a00dc3c3a97aa724d015aa06c7283a33f
Nicole <petovellon@gmail.com>
2023-08-02 09:56:56 +0200

    Fixed responsiveness on loading bar

--- src/components/loading-bar/LoadingBar.tsx
+++ src/components/loading-bar/LoadingBar.tsx
@@ -1,12 +1,18 @@
 import { LoadingBarProps } from '../../types/LoadingBarProps';
 import './LoadingBar.css';
 
+
+/**
+ * I did not pass the css classname directly through props since it depends conditionally of whether the 
+ * progress is complete or not .
+ */
 const LoadingBar: React.FC<LoadingBarProps> = (props: LoadingBarProps) => {
 	return (
 		<div className="loading-bar-container">
 			<p>{props.recipeName}</p>
 			<div className='loading-bar-wrapper'>
-				<div className='loading-progress' style={{
+				
+				<div className={props.progressPercent !== 100 ? 'loading-progress' : 'loading-complete'} style={{
 					width: `${props.progressPercent?.toString()}%`
 				}}>
 				</div>
