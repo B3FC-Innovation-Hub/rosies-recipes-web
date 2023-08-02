e6e35bb01705e344b3b649a91c62476d62f02508
Nicole <petovellon@gmail.com>
2023-08-02 11:22:28 +0200

    Fixed formatting

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,24 +1,22 @@
+import { ModalProps } from "../../types/ModalProps";
+import Button from "../button/Button";
+import "./Modal.css";
+import close from "../../assets/icons/icons8-close-24.png";
 
-import { ModalProps } from '../../types/ModalProps';
-import Button from '../button/Button';
-import './Modal.css';
-import close from '../../assets/icons/icons8-close-24.png';
-
-const Modal:React.FC<ModalProps> = (props: ModalProps) => {
-	return(
+const Modal: React.FC<ModalProps> = (props: ModalProps) => {
+	return (
 		<>
 			<div className={`modal-container ${props.cssClass}`}>
+				{props.toggable && (
+					<div className="modal-button-wrapper">
+						<Button color="pink" icon={close} cssClass="fab" />
+					</div>
+				)}
+				<h2 className="modal-title">{props.modalTitle}</h2>
 				{
-					props.toggable && <div className='modal-button-wrapper'><Button color='pink' icon={close} cssClass='fab'/></div>
+					//props.children helps us to dynamically render children react components depending on our needs
 				}
-				<h2 className='modal-title'>{props.modalTitle}</h2>
-				{
-				//props.children helps us to dynamically render children react components depending on our needs
-				}  
-				<section className='modal-content-section'>
-					{props.children}
-				</section>
-
+				<section className="modal-content-section">{props.children}</section>
 			</div>
 		</>
 	);
