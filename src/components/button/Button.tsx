cbac8013026490cdc6ec4879459b64139a3d8281
Nicole <petovellon@gmail.com>
2023-08-02 09:58:49 +0200

    Changed name of button property

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,7 +3,7 @@ import "./Button.css";
 
 const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
 	return (
-		<button aria-label="cta button" type="button" className={`button ${props.color} ${props.fab ?? ''}`} onClick={props.onClick}>
+		<button aria-label="cta button" type="button" className={`button ${props.color} ${props.cssClass ?? ''}`} onClick={props.onClick}>
 			{
 				props.icon && <img className="btn-icon" src={props.icon} alt="an icon"/>
 			}
