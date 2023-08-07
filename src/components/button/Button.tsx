c216f561eff42ed600e000d58409191ab4806cfa
Nicole <petovellon@gmail.com>
2023-08-07 13:14:57 +0200

    Removed img to render svg icon directly as jsx

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,7 +5,7 @@ const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
 	return (
 		<button aria-label="cta button" type="button" className={`button ${props.color} ${props.cssClass ?? ''}`} onClick={props.onClick}>
 			{
-				props.icon && <img className="btn-icon" src={props.icon} alt="an icon"/>
+				props.icon ?? <></>
 			}
 			<>{props.title ?? <></>}</>
 		</button>
