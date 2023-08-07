a7ea7e91afb4ac4ab4b27f57eb151de561226f7a
Nicole <petovellon@gmail.com>
2023-08-07 13:14:27 +0200

    Updated icon in button prop

--- src/types/ButtonProps.ts
+++ src/types/ButtonProps.ts
@@ -1,8 +1,10 @@
+import { ReactComponentElement } from "react";
+
 export type ButtonProps = {
     title?: string;
     onClick?: () => void | Promise<void>; // optional, but recommended to handle async operations
     //To specify which color the button will be
     color: string;
     cssClass?: string;
-    icon?: string;
+    icon?: ReactComponentElement<any, any>;
 }
