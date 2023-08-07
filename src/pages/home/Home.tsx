9a705f5056bb8c1fd309f07f4d122d24568d0de3
Nicole <petovellon@gmail.com>
2023-08-07 16:16:41 +0200

    Updated jsx components with comments and svg icons

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,9 +2,11 @@ import { useNavigate } from "react-router-dom";
 import Button from "../../components/button/Button";
 import TopBar from "../../components/topbar/TopBar";
 import { AuthenticatedTemplate, useMsal } from "@azure/msal-react";
-import './Home.css';
+import "./Home.css";
 import { RedirectRequest } from "@azure/msal-browser";
-import './Home.css';
+import { ReactComponent as SearchIcon } from "../../assets/icons/search-icon.svg";
+import { ReactComponent as UploadIcon } from "../../assets/icons/upload-icon.svg";
+import "./Home.css";
 import HomeBackground from "./home-bg/HomeBackground";
 import Footer from "../../components/footer/Footer";
 
@@ -12,12 +14,12 @@ const Home: React.FC = () => {
 	const navigate = useNavigate();
 	const { instance, accounts } = useMsal();
 	const redirectStartPage = process.env.REACT_APP_DEV_URI;
-	
+
 	const request: RedirectRequest = {
 		scopes: [],
-		redirectStartPage: redirectStartPage
+		redirectStartPage: redirectStartPage,
 	};
-	
+
 	const logoutHandler = () => {
 		try {
 			instance.loginRedirect(request);
@@ -30,15 +32,15 @@ const Home: React.FC = () => {
 			<TopBar />
 			<div className="home-container">
 				<HomeBackground />
-	
+
 				<div className="home-container-content">
 					<h1 className="home-header">
-                Your meal starts <strong>here </strong>
+            Your meal starts <strong>here </strong>
 					</h1>
 					<p className="home-content">
-               				Give me your best recipes, or browse recipes from my collection and 
-				I will provide optimized step-by-step, optimized instructions,
-				so that you do not waste any time .
+            Give me your best recipes, or browse recipes from my collection and
+            I will provide optimized step-by-step, optimized instructions, so
+            that you do not waste any time .
 						<AuthenticatedTemplate>
 							<p>Signed in as: {accounts[0]?.username}</p>
 						</AuthenticatedTemplate>
@@ -50,8 +52,18 @@ const Home: React.FC = () => {
 					</p>
 				</div>
 				<div className="button-row">
-					<Button title="Upload" color="pink" onClick={() => navigate('/')}/>
-					<Button title="Browse" color="pink" onClick={() => navigate('/')}/>
+					<Button
+						title="Upload"
+						color="pink"
+						onClick={() => navigate("/")}
+						icon={<SearchIcon fill="white" />}
+					/>
+					<Button
+						title="Browse"
+						color="pink"
+						onClick={() => navigate("/")}
+						icon={<UploadIcon fill="white" />}
+					/>
 				</div>
 				<Footer />
 			</div>
