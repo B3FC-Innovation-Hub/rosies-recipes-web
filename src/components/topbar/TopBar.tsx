2ebb66f07ac79a1ac2bfc62f134209ab654ab557
Nicole <petovellon@gmail.com>
2023-08-09 16:35:31 +0200

    Added logout button and username to topbar

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,21 +1,51 @@
 import './TopBar.css';
 import { ReactComponent as MenuIcon } from "../../assets/icons/menu-icon.svg";
-import { ReactComponent as AboutIcon } from "../../assets/icons/about-icon.svg";
-import { ReactComponent as HomeIcon } from "../../assets/icons/home-icon.svg";
-import { ReactComponent as ReloadIcon } from "../../assets/icons/reload-icon.svg";
+import { TopBarProps } from '../../types/TopBarProps';
+import { Outlet } from 'react-router';
+import { useMsal } from '@azure/msal-react';
+import { RedirectRequest } from '@azure/msal-browser';
+import Button from '../button/Button';
 
-const TopBar: React.FC = () => {
+const TopBar: React.FC<TopBarProps> = (props: TopBarProps) => {
+	const { instance } = useMsal();
+	const redirectStartPage = process.env.REACT_APP_DEV_URI;
+
+	const request: RedirectRequest = {
+		scopes: [],
+		redirectStartPage: redirectStartPage,
+	};
+
+	const logoutHandler = () => {
+		try {
+			instance.logoutRedirect(request);
+		} catch (e) {
+			console.log(e);
+		}
+	};
 	return (
-		<nav className='top-bar-container'>
-			<div className='menu-left'>
-				<MenuIcon className='topbar-icon' fill='white'/>
-			</div>
-			<div className='menu-right'>
-				<AboutIcon className='topbar-icon' fill='white'/>
-				<HomeIcon className='topbar-icon' fill='white'/>
-				<ReloadIcon className='topbar-icon' fill='white'/>
-			</div>
-		</nav>
+		<div className='top-bar-wrapper-container'>
+			<nav className='top-bar-container'>
+				<div className='menu-left'>
+					{
+						/*
+						Inside this burger menu, we'll have a custom collapsible menu
+						*/
+					}
+					<MenuIcon className='topbar-icon' fill='white'/>
+				</div>
+				<div className='menu-right'>
+					<p className='topbar-usernamee'>Signed in as {props.username}</p>
+					<div>
+						<Button
+							title="Sign out"
+							color="green"
+							onClick={() => logoutHandler()}
+						/>
+					</div>
+				</div>
+			</nav>
+			<Outlet />
+		</div>
 	);
 };
 
