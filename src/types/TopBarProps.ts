e26c7f56a604fd282fb772139ab54c31d417ce5e
Nicole <petovellon@gmail.com>
2023-08-09 16:37:54 +0200

    Added props to topbar and passed username to topbar

---
+++ src/types/TopBarProps.ts
@@ -0,0 +1,6 @@
+export type TopBarProps = {
+    /*
+    This will be the email/name of the logged account. We can use this type to add more props in case we need in the future.
+    */
+	username: string;
+}
