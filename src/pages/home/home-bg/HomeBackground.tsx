c030e34d6f83320cf2c9513c56a18008fd7e0bb3
Nicole <petovellon@gmail.com>
2023-08-14 10:41:01 +0200

    Created variables for the background spheres

--- src/pages/home/home-bg/HomeBackground.tsx
+++ src/pages/home/home-bg/HomeBackground.tsx
@@ -4,14 +4,15 @@ import Sphere from '../../../components/sphere/Sphere';
 import droolingFace from '../../../assets/emoji-assets/drooling-face.png';
 import cheesecake from '../../../assets/food-assets/cheesecake.png';
 import spaghetti from '../../../assets/food-assets/spaghetti.png';
+import { HomeBackgroundPalette } from './HomeBackgroundSpheresPalette';
 
 const HomeBackground: React.FC = () => {
 	return (
 		<div className='stripes-container'>
 			<div className='images-container'>
-				<Sphere src={droolingFace} height={200} width={200} backgroundColor='#DBFE87' top={4} right={2}/>
-				<Sphere src={spaghetti} height={150} width={150} backgroundColor='#EFB9F3' top={40} right={10} />
-				<Sphere src={cheesecake} height={120} width={120} backgroundColor='#FFA56E' bottom={10} right={2} />
+				<Sphere src={droolingFace} height={200} width={200} backgroundColor={HomeBackgroundPalette.secondary} top={4} right={2}/>
+				<Sphere src={spaghetti} height={150} width={150} backgroundColor={HomeBackgroundPalette.accent} top={40} right={10} />
+				<Sphere src={cheesecake} height={120} width={120} backgroundColor={HomeBackgroundPalette.warn} bottom={10} right={2} />
 			</div>
 			<div className='stripes'>
 				<div className='stripes darker'>
