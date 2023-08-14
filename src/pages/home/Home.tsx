d3dd896b633b47a3ec280c742f65d408a3cef079
Nicole <petovellon@gmail.com>
2023-08-14 15:19:30 +0200

    Corrected cs selector + added route

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,7 +11,7 @@ const HomePageFC: React.FC = () => {
 	const navigate = useNavigate();
 	return (
 		<>
-			<div className="wrapper">
+			<div className="home-wrapper">
 				<HomeBackground />
 				<div className="home-container">
 					<UnauthenticatedTemplate>
@@ -34,7 +34,7 @@ const HomePageFC: React.FC = () => {
 							<Button
 								title="Upload"
 								color="accent"
-								onClick={() => navigate("/")}
+								onClick={() => navigate("/upload")}
 								icon={<SearchIcon />}
 							/>
 							<Button
