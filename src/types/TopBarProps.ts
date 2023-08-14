0cb16654886c9ef50f105b6262656c5f67ded7f6
Nicole <petovellon@gmail.com>
2023-08-14 15:27:59 +0200

    Removed whitespace, added comments

--- src/types/TopBarProps.ts
+++ src/types/TopBarProps.ts
@@ -2,5 +2,8 @@ export type TopBarProps = {
     /*
     This will be the email/name of the logged account. We can use this type to add more props in case we need in the future.
     */
-	username: string;
+   username?: string;
+    /**  In case we want to pass down the name,
+     the property name comes from MSAL as string | undefined */
+    name?: string | undefined; 
 }
