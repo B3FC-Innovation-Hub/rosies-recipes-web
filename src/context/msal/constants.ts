40ef3e67e63f9d2cc90bda263dc729eea483df60
Nicole <petovellon@gmail.com>
2023-09-01 11:40:38 +0200

    test

--- src/context/msal/constants.ts
+++ src/context/msal/constants.ts
@@ -1,9 +1,12 @@
 import { Configuration, PublicClientApplication } from "@azure/msal-browser";
 
+/**TODO: delete this */
+console.log(process.env.NODE_ENV);
 
 const tenantId = process.env.REACT_APP_TENANT_ID;
 const clientId = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? process.env.REACT_APP_DEV_CLIENT_ID : process.env.REACT_APP_PROD_CLIENT_ID;
 const url = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development')? process.env.REACT_APP_DEV_URI : process.env.REACT_APP_PROD_URI;
+
 /*
 	Both client id and tenant id can be found in Azure's App Registrations 
 */
